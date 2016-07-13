208298af2af111354ba5ea441775f33587b06ca2
SABARISH <RSABA@monsanto.com>
2016-07-13 18:34:12 +0530

    Mean Calculation

--- src/ParseData.scala
+++ src/ParseData.scala
@@ -1,14 +1,28 @@
-import scala.collection.mutable.ArrayBuffer
-
+import scala.collection.mutable.ListBuffer
 
 object ParseData {
-  def main( args: Array[String]) = {
-    val bufferedSource = io.Source.fromFile("C:/Sabarish/Data/WeatherDataset_Training.csv")
-      for (line <- bufferedSource.getLines) {
-          val cols = line.split(",").map(_.trim)
-          // do whatever you want with the columns here
-          println(s"${cols(11)}|${cols(10)}|${cols(2)}|${cols(3)}")
-      }
-      bufferedSource.close
+  var col = 0.0
+  val alpha = 0.5
+  def main(args: Array[String]) = {
+    var temp = new ListBuffer[Float]()
+    // Locate file location for your DataSet
+    val bufferedSource = io.Source.fromFile("C:/Users/RSABA/workspace/WeatherAnalysis/src/WeatherDataset_Final.csv")
+    println("WeatherStation Year-Month Temp  Pressure")
+    for (line <- bufferedSource.getLines) {
+      val cols = line.split(",").map(_.trim)
+      println(cols { (11) } + "|" + cols { (10) } + "|" + cols { (1) } + "|" + cols { (4) } + "|" + cols { (5) })
+      col = cols { (2) }.toDouble
+      temp += col.toFloat
+    }
+    println("____________________________________________________________________")
+    println(mean(temp))
+    bufferedSource.close
   }
+
+  def mean(xs: Iterable[Float]) = xs.sum / xs.size
+
+  def medianFilteredCities() = {
+    //Exponential Smoothening
+  }
+
 }
